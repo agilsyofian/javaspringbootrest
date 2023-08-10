8cf571dbe5635bede9a4e9c18c8ac98c329a6aa0
ash <agil.hidayat@ist.id>
2023-08-10 10:17:44 +0700

    dev test commit 2

--- src/test/java/ist/challenge/agil_syofian_hidayat/AgilSyofianHidayatApplicationTests.java
+++ src/test/java/ist/challenge/agil_syofian_hidayat/AgilSyofianHidayatApplicationTests.java
@@ -1,13 +1,7 @@
 package ist.challenge.agil_syofian_hidayat;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class AgilSyofianHidayatApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
 }
